3da8bca61c5cc9aa5b3b624ff8aec1d4083a3f66
kjh970519 <115525671+kjh970519@users.noreply.github.com>
2025-01-15 14:23:15 +0900

    send_webhook

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
+    $send_data = $_REQUEST['send_data'];
 
-$curl = curl_init();
+    if (isset($_REQUEST['send_data'])) {
+        $data = [
+            "content" => "**[ {$send_data['ticker']} ]** [{$send_data['subject']}]({$send_data['url']})",
+            "username" => "stock-new-bot", // 선택사항: 웹훅에 표시될 사용자 이름
+            "avatar_url" => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ecGI6G--McHmbv95b61RDQbL7ZggrsQ4IA&s', // 봇의 프로필 이미지 URL
+        ];
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://discord.com/api/webhooks/1328597708553846785/jQzkSmExZpaw72vyc-nNL3LTHXdwvTlRqAIgsTNHokyckjuXVaFovqp2sUFLhXelaaQe',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS =>'{"content":"**[ LAES, LAESV ]** [SEALSQ in Cooperation with WISeKey Expands Post-Quantum Footprint in Saudi Arabia](https:\\/\\/www.stocktitan.net\\/news\\/LAES\\/sealsq-in-cooperation-with-wi-se-key-expands-post-quantum-footprint-8t64vsnpc29b.html)","username":"stock-new-bot","avatar_url":"https:\\/\\/encrypted-tbn0.gstatic.com\\/images?q=tbn:ANd9GcQ6ecGI6G--McHmbv95b61RDQbL7ZggrsQ4IA&s"}',
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json',
-        'Cookie: __cfruid=aa297349782f364b166bd43d6d0f8f1cf2da9ec8-1736843886; __dcfduid=e03a12bcd25211ef9856ba35e2a72568; __sdcfduid=e03a12bcd25211ef9856ba35e2a72568f2d4edc37bb8c884f68e1ef77e73e4b2cbacddcf1882f6dd71c3b7cf22aef6f3; _cfuvid=iNOCpsg1.HQVsS2NUcHaWfrDujO2RxRrVdYUz9YJyuA-1736843886380-0.0.1.1-604800000'
-    ),
-));
+        $json_data = json_encode($data);
 
-$response = curl_exec($curl);
+        $ch = curl_init($webhook_url);
 
-curl_close($curl);
-echo $response;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        if ($response === false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+?>
